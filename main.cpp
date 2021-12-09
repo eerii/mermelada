8569452919f39233d8c945fcd78365860b783ce4
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-12-09 14:03:46 +0100

    Playground 🦋

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  aguacate
+//
+//  Created by José on 9/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
