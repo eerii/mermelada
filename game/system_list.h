e8d1a108b35db8bfb2061885e3b9fbcc8222e0ba
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2021-12-23 21:35:57 +0100

    Camera Improvements 🎥

--- game/system_list.h
+++ game/system_list.h
@@ -7,6 +7,7 @@
 #include "scene.h"
 
 #include "log.h"
+#include "input.h"
 #include "r_graphics.h"
 #include "f_time.h"
 
@@ -27,18 +28,30 @@ namespace Fresa::System
             for (EntityID e : SceneView<Component::Test>(s)) {
                 log::info("%d %s", e, s.getName(e).c_str());
             }*/
+            
+            //: Sample camera update
+            if (Input::keyboard_down(119))
+                Graphics::camera.pos.y += 1.0f * Time::physics_delta;
+            if (Input::keyboard_down(115))
+                Graphics::camera.pos.y -= 1.0f * Time::physics_delta;
+            if (Input::keyboard_down(100))
+                Graphics::camera.pos.x += 1.0f * Time::physics_delta;
+            if (Input::keyboard_down(97))
+                Graphics::camera.pos.x -= 1.0f * Time::physics_delta;
         }
         
         inline static void render() {
             float t = sec(time() - start_time);
             
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f + 0.8f * std::sin(t * 1.570796f)));
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -1.0f + 0.8f * std::sin(t * 1.570796f), 0.0f));
             model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
+            model = glm::rotate(model, -1.570796f, glm::vec3(1.0f, 0.0f, 0.0f));
             model = glm::rotate(model, t * 1.570796f, glm::vec3(0.0f, 0.0f, 1.0f));
             Graphics::draw(test_draw_id, model);
             
-            glm::mat4 model2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 1.0f + 0.8f * std::sin(t * 1.570796f + 1.570796f)));
+            glm::mat4 model2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.0f + 0.8f * std::sin(t * 1.570796f + 1.570796f), 0.0f));
             model2 = glm::scale(model2, glm::vec3(0.5f, 0.5f, 0.5f));
+            model2 = glm::rotate(model2, 1.570796f, glm::vec3(1.0f, 0.0f, 0.0f));
             model2 = glm::rotate(model2, -t * 1.570796f, glm::vec3(0.0f, 0.0f, 1.0f));
             Graphics::draw(test_draw_id_2, model2);
         }
