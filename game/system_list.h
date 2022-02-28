565acb8f3845044b156bb1671eee542e0f442c0c
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2022-02-28 20:43:48 +0100

    Improved Renderer Description 📝

--- game/system_list.h
+++ game/system_list.h
@@ -11,16 +11,18 @@
 #include "r_graphics.h"
 #include "f_time.h"
 
+using namespace Fresa::Graphics;
+
 namespace Fresa::System
 {
     //: Example of a hardcoded render system
     //: This is very basic and not elegant, it will be reworked in the future
 
-    inline Graphics::TextureID texture_data;
-    inline Graphics::DrawID draw_id_tex_a;
-    inline Graphics::DrawID draw_id_tex_b;
-    inline std::array<Graphics::DrawID, 20> draw_ids_cube{};
-    inline std::array<Graphics::DrawID, 16> draw_ids_floor{};
+    inline TextureID texture_data;
+    inline DrawID draw_id_tex_a;
+    inline DrawID draw_id_tex_b;
+    inline std::array<DrawID, 20> draw_ids_cube{};
+    inline std::array<DrawID, 16> draw_ids_floor{};
     
     inline Clock::time_point start_time = time();
     
@@ -28,11 +30,11 @@ namespace Fresa::System
     inline std::array<glm::vec3, 20> rotations{};
     inline std::array<glm::vec3, 16> positions_floor{};
     
-    inline const std::vector<Graphics::Projection> projections = {
-        Graphics::PROJECTION_ORTHOGRAPHIC,
-        Graphics::Projection(Graphics::PROJECTION_ORTHOGRAPHIC | Graphics::PROJECTION_SCALED),
-        Graphics::PROJECTION_PERSPECTIVE,
-        Graphics::Projection(Graphics::PROJECTION_PERSPECTIVE | Graphics::PROJECTION_SCALED),
+    inline const std::vector<Projection> projections = {
+        PROJECTION_ORTHOGRAPHIC,
+        Projection(PROJECTION_ORTHOGRAPHIC | PROJECTION_SCALED),
+        PROJECTION_PERSPECTIVE,
+        Projection(PROJECTION_PERSPECTIVE | PROJECTION_SCALED),
     };
     inline int proj_i = 0;
     
@@ -45,47 +47,47 @@ namespace Fresa::System
             
             //: Sample camera update
             if (Input::key_down(SDLK_d))
-                Graphics::camera.pos.x += 300.0f * Time::physics_delta;
+                camera.pos.x += 300.0f * Time::physics_delta;
             if (Input::key_down(SDLK_a))
-                Graphics::camera.pos.x -= 300.0f * Time::physics_delta;
+                camera.pos.x -= 300.0f * Time::physics_delta;
             
-            if (Graphics::camera.proj_type & Graphics::PROJECTION_PERSPECTIVE) {
+            if (camera.proj_type & PROJECTION_PERSPECTIVE) {
                 if (Input::key_down(SDLK_q))
-                    Graphics::camera.pos.y += 300.0f * Time::physics_delta;
+                    camera.pos.y += 300.0f * Time::physics_delta;
                 if (Input::key_down(SDLK_e))
-                    Graphics::camera.pos.y -= 300.0f * Time::physics_delta;
+                    camera.pos.y -= 300.0f * Time::physics_delta;
                 if (Input::key_down(SDLK_w))
-                    Graphics::camera.pos.z -= 300.0f * Time::physics_delta;
+                    camera.pos.z -= 300.0f * Time::physics_delta;
                 if (Input::key_down(SDLK_s))
-                    Graphics::camera.pos.z += 300.0f * Time::physics_delta;
+                    camera.pos.z += 300.0f * Time::physics_delta;
             } else {
                 if (Input::key_down(SDLK_w))
-                    Graphics::camera.pos.y -= 300.0f * Time::physics_delta;
+                    camera.pos.y -= 300.0f * Time::physics_delta;
                 if (Input::key_down(SDLK_s))
-                    Graphics::camera.pos.y += 300.0f * Time::physics_delta;
+                    camera.pos.y += 300.0f * Time::physics_delta;
             }
             
             if (Input::key_pressed(SDLK_TAB)) {
                 proj_i = (proj_i + 1) % projections.size();
-                Graphics::camera.proj_type = projections.at(proj_i);
-                Graphics::updateCameraProjection(Graphics::camera);
-                Graphics::win.scaled_ubo = (Graphics::camera.proj_type & Graphics::PROJECTION_SCALED) ?
-                                            Graphics::API::getScaledWindowUBO(Graphics::win) :
-                                            Graphics::UniformBufferObject{glm::mat4(1.0f),glm::mat4(1.0f),glm::mat4(1.0f)};
+                camera.proj_type = projections.at(proj_i);
+                updateCameraProjection(camera);
+                win.scaled_ubo = (camera.proj_type & PROJECTION_SCALED) ?
+                                            API::getScaledWindowUBO(win) :
+                                            UniformBufferObject{glm::mat4(1.0f),glm::mat4(1.0f),glm::mat4(1.0f)};
             }
         }
         
         inline static void render() {
             static bool init = false;
             if (not init) {
-                texture_data = Graphics::getTextureID(File::path("graphics/texture.png"));
-                draw_id_tex_a = Graphics::getDrawID_Rect(Graphics::SHADER_DRAW_TEX);
-                Graphics::bindTexture(draw_id_tex_a, texture_data);
-                draw_id_tex_b = Graphics::getDrawID_Rect(Graphics::SHADER_DRAW_TEX);
-                Graphics::bindTexture(draw_id_tex_b, texture_data);
+                texture_data = getTextureID(File::path("graphics/texture.png"));
+                draw_id_tex_a = getDrawID(Vertices::rect3_tex, Indices::rect, "draw_tex");
+                bindTexture(draw_id_tex_a, texture_data);
+                draw_id_tex_b = getDrawID(Vertices::rect3_tex, Indices::rect, "draw_tex");
+                bindTexture(draw_id_tex_b, texture_data);
                 
                 for (int i = 0; i < draw_ids_cube.size(); i++) {
-                    draw_ids_cube[i] = Graphics::getDrawID_Cube(Graphics::SHADER_DRAW_COLOR);
+                    draw_ids_cube[i] = getDrawID(Vertices::cube_color, Indices::cube, "draw_color");
                     float theta_p = float(rand() % 31415) / 10000.0f; //: 0, pi
                     float phi_p = float(rand() % 62829) / 10000.0f; //: 0, 2*pi
                     positions[i] = glm::vec3(std::cos(theta_p) * std::sin(phi_p), std::sin(theta_p) * std::sin(phi_p), std::cos(phi_p));
@@ -98,12 +100,12 @@ namespace Fresa::System
                 for (int i = -2; i < 2; i++) {
                     for (int j = -2; j < 2; j++) {
                         int k = (i+2) + 4*(j+2);
-                        draw_ids_floor[k] = Graphics::getDrawID_Cube(Graphics::SHADER_DRAW_COLOR);
+                        draw_ids_floor[k] = getDrawID(Vertices::cube_color, Indices::cube, "draw_color");
                         positions_floor[k] = glm::vec3(200.0f * i + 100.0f, 250.0f, 200.0f * j + 100.0f);
                     }
                 }
                 
-                Graphics::camera.pos.z = 500.0f;
+                camera.pos.z = 500.0f;
                 
                 init = true;
             }
@@ -115,13 +117,13 @@ namespace Fresa::System
             model = glm::scale(model, glm::vec3(1.0f) * 100.0f);
             model = glm::rotate(model, t * 3.141592f, glm::vec3(0.0f, 1.0f, 0.0f));
             model = glm::translate(model, glm::vec3(-0.5f, 0.0f, 0.0f));
-            Graphics::draw(draw_id_tex_a, model);
+            draw(draw_id_tex_a, model);
             #ifndef USE_OPENGL
             model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 100.0f * std::sin(t * 1.570796f), -100.0f));
             model = glm::scale(model, glm::vec3(1.0f) * 100.0f);
             model = glm::rotate(model, (t + 1.0f) * 3.141592f, glm::vec3(0.0f, 1.0f, 0.0f));
             model = glm::translate(model, glm::vec3(-0.5f, 0.0f, 0.0f));
-            Graphics::draw(draw_id_tex_b, model); //Quick hack to avoid disabling culling
+            draw(draw_id_tex_b, model); //Quick hack to avoid disabling culling
             #endif
             
             //: Cubes
@@ -129,14 +131,14 @@ namespace Fresa::System
                 model = glm::translate(glm::mat4(1.0f), positions[i]);
                 model = glm::scale(model, glm::vec3(1.0f) * 10.0f);
                 model = glm::rotate(model, t * 1.570796f, rotations[i]);
-                Graphics::draw(draw_ids_cube[i], model);
+                draw(draw_ids_cube[i], model);
             }
             
             //: Floor
             for (int i = 0; i < draw_ids_floor.size(); i++) {
                 model = glm::translate(glm::mat4(1.0f), positions_floor[i]);
                 model = glm::scale(model, glm::vec3(100.0f, 30.0f, 100.0f));
-                Graphics::draw(draw_ids_floor[i], model);
+                draw(draw_ids_floor[i], model);
             }
         }
     };
---
+++ game/vertex_list.h
@@ -0,0 +1,17 @@
+//project verse, 2017-2022
+//by jose pazos perez
+//licensed under GPLv3
+
+//: Optional file to define new vertex definitions for shaders
+//: It is included in "r_dtypes.h"
+
+#pragma once
+
+#include "reflection.h"
+
+struct VertexExample {
+    Serialize(VertexExample, pos);
+    glm::vec3 pos;
+};
+    
+using CustomVertexType = std::variant<VertexExample>;
