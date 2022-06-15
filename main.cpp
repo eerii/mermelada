fecfb864309bc1f27ddded691982b0252c81ea89
josekoalas <jose.pazos.perez@rai.usc.es>
2022-06-15 11:47:20 +0200

    Engine 🍋

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 //* fresa, a tiny engine by jose pazos perez
 
-#include "fresa_types.h"
-#include "fresa_test.h"
-
-using namespace fresa;
-
-inline TestSuite suite("Some tests", []{
-    "Test one"_test = []{ return expect(1 == 1); };
-    "Test two"_test = []{ return expect(1 == 2); };
-});
+#include "engine.h"
 
 int main (int argv, char** args) {
-    test_runner.run();
-
+    fresa::run();
     return 0;
 }
