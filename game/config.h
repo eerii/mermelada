69d0c696de448178427f28fb3d9991ab6ca39f4a
josekoalas <jose.pazos.perez@rai.usc.es>
2022-07-03 15:31:42 +0200

    Improved Configuration 🦎

--- game/config.h
+++ game/config.h
@@ -1,15 +1,24 @@
 //* game_config
 //      sample configuration for a game, extends fresa_config.h
-//      works by overriding the pure constexpr virtual functions (a feature of c++20)
-//      that way, all configuration is resolved at compile time but can be modified here
 #pragma once
 
 #include "fresa_config.h"
 
 namespace fresa
 {
-    constexpr inline struct GameConfig : Config {
-        //: change the project name to...
+    //* engine config
+    //      compile time, modify default values using overrides of the constexpr virtual functions
+    constexpr inline struct _EngineConfig : EngineConfig {
         constexpr str_view name() const override { return "fresa"; }
-    } config;
+    } engine_config;
+
+    //* run config
+    //      run time, modify default values using the constructor
+    inline RunConfig config{};
+
+    //* debug config
+    //      run time, only on debug builds, same structure as run config
+    #ifdef DEBUG
+    inline DebugConfig debug_config{};
+    #endif
 }
