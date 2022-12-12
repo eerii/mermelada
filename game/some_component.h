eabdfbd9af501c1a6bace321136a50b61d8858df
josekoalas <jose.pazos.perez@rai.usc.es>
2022-12-12 15:39:05 +0100

    Clang Plugin 🐠

--- game/some_component.h
+++ game/some_component.h
@@ -13,11 +13,3 @@ namespace fresa
         std::vector<str> some_vector;
     };
 }
-
-//* add reflection
-//      to add reflection information, include the file "reflection_<filename>.h"
-//      this will trigger the automatic generator when building the code
-//      the include has to be at the end of the file
-#if __has_include("reflection_some_component.h")
-#include "reflection_some_component.h"
-#endif
